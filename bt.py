8cb3d3f1ef3239e493991c2b7c5f40d1c42fb399
dagmawim <dmm474@drexel.edu>
2017-07-04 12:36:11 -0400

    bluetooth init

--- bt.py
+++ bt.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+# simple inquiry example
+import bluetooth
 
-print "Hey"
+nearby_devices = bluetooth.discover_devices(lookup_names=True)
+print("found %d devices" % len(nearby_devices))
 
+for addr, name in nearby_devices:
+    print("  %s - %s" % (addr, name))
