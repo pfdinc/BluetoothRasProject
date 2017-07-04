364899f42a52d58971ffe6102d8e7479d8680318
dagmawim <dmm474@drexel.edu>
2017-07-04 13:00:19 -0400

    more mod

--- bt.py
+++ bt.py
@@ -2,9 +2,15 @@
 # simple inquiry example
 import bluetooth
 
-print "Running...."
-nearby_devices = bluetooth.discover_devices(lookup_names=True)
+print("performing inquiry...")
+
+nearby_devices = bluetooth.discover_devices(
+        duration=8, lookup_names=True, flush_cache=True, lookup_class=False)
+
 print("found %d devices" % len(nearby_devices))
 
 for addr, name in nearby_devices:
-    print("  %s - %s" % (addr, name))
+    try:
+        print("  %s - %s" % (addr, name))
+    except UnicodeEncodeError:
+        print("  %s - %s" % (addr, name.encode('utf-8', 'replace')))
